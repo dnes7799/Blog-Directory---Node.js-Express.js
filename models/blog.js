df5daa954bb89e235504d0a4f320b23d6987e747
Dinesh Khadka <51894957+dnes7799@users.noreply.github.com>
2022-02-21 13:11:58 +0545

    Blog model

---
+++ models/blog.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//defning a schema
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+//model for blogSchema
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports = Blog;
